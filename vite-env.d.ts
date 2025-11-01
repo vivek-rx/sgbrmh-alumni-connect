a2f1d1da6cfed6851078b3c9642697ff0d20b569
Vivek Randad <145924364+vivek-rx@users.noreply.github.com>
2025-11-02 04:49:07 +0530

    structured repo

---
+++ vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+  // more env variables...
+  readonly [key: string]: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
